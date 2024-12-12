3fcc8ab97400576ec9112c8b8a1d604e3c1eb4cb
NYSBC-Rudra <133771854+NYSBC-Rudra@users.noreply.github.com>
2024-12-12 15:04:02 -0500

    Update dewar.py

--- gui/dialog/dewar.py
+++ gui/dialog/dewar.py
@@ -22,7 +22,7 @@ class DewarDialog(QtWidgets.QDialog):
     def initData(self):
         dewarObj = self.connection.getFromRedis('NyxDewar')
         dewarObj = eval(dewarObj)
-        if dewarObj is '':
+        if dewarObj == '':
             dewarObj = {"content": [""] * (self.pucksPerDewarSector * self.dewarSectors), 'name': 'NyxDewar', 'pucks':[]}
         puckLocs = dewarObj["content"]
         #[''*28]
